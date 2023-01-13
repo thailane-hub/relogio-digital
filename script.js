d4e859dbdb57f54c501e7ffcb5f9f3cb0810eaa2
thailane <thailane.araujo@gmail.com>
2023-01-12 22:40:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) mim = '0' + min;
+    if (seg < 10) seg = '0' + seg;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+
+})
